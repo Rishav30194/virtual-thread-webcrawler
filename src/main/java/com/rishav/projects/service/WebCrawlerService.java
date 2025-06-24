fee0351bf3fa0e35a14a8fff619e90f38c5cf355
Rishavsingh Kshatriya <rishav30194@gmail.com>
2025-06-24 15:08:39 -0400

    added http core

--- src/main/java/com/rishav/projects/service/WebCrawlerService.java
+++ src/main/java/com/rishav/projects/service/WebCrawlerService.java
@@ -2,6 +2,16 @@ package com.rishav.projects.service;
 
 import com.rishav.projects.model.CrawlResult;
 import org.apache.hc.client5.http.classic.HttpClient;
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.HttpEntity;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Service class for web crawling operations.
@@ -9,6 +19,7 @@ import org.apache.hc.client5.http.classic.HttpClient;
  */
 public class WebCrawlerService {
 
+    Logger logger = Logger.getLogger(WebCrawlerService.class.getName());
     /**
      * Crawls the specified URL.
      * This method is a placeholder and does not perform any actual crawling.
@@ -18,8 +29,24 @@ public class WebCrawlerService {
     public CrawlResult crawlUrl(String url) {
 
         CrawlResult result = new CrawlResult();
-        HttpClient httpClient;
-
+        HttpGet httpGet = new HttpGet(url);
+        try(CloseableHttpClient httpClient = HttpClients.createDefault()) {
+            try(CloseableHttpResponse response = httpClient.execute(httpGet)) {
+                logger.log(Level.INFO, String.valueOf(response.getCode()));
+                logger.log(Level.INFO, response.getReasonPhrase());
+                HttpEntity entity = response.getEntity();
+                if(entity != null){
+                    result.setStatus("Crawled successfully");
+                    result.setUrl(url);
+                    result.setContent(entity.getContent().toString());
+                } else {
+                    result.setStatus("No content found for the URL: " + url);
+                }
+            }
+        }catch (IOException e) {
+            result.setStatus("Error: " + e.getMessage());
+            return result;
+        }
 
         return result;
     }
